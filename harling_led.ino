460a083dee10e2fcc315abb86a7a8d24751231bb
StefanHaslhofer <haslhofer.stefan@gmail.com>
2022-10-11 19:59:32 +0200

    functioning wave

--- harling_led.ino
+++ harling_led.ino
@@ -1,13 +1,17 @@
 #include <Adafruit_NeoPixel.h>
 
 #define LED_PIN   6  // any PWM capable pin
-#define NUM_LEDS 79
-#define WAVE_LENGTH 10
+#define AUDIO_PIN 0  // input pin for audio signal 
 
-#define DEBUG_MSG true
+#define NUM_LEDS 79
+#define WAVE_LENGTH 30
+#define COLOR_MAX 150
+#define MAX_DELAY 150
+
+#define DEBUG_MSG false
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_RGBW + NEO_KHZ800);
-uint8_t lights[NUM_LEDS];
+uint32_t lights[NUM_LEDS];
 
 void setup()
 {
@@ -18,30 +22,36 @@ void setup()
 
 void loop()
 {
-  //showMeteorPixels(1, 10, strip.Color(0, 150, 0));
-  showWave(strip.Color(0, 150, 0));
+  showWave(strip.Color(20, COLOR_MAX, 20));
 }
 
 void showWave(uint32_t color) {
+  int audioPressure = 1;
+  int waveIndex = 0;
+  bool showColor = true;
+  
   #if DEBUG_MSG
     Serial.println("START WAVE");
   #endif
   
   turnOffPixels();
-  
-  int waveIndex = 0;
-  bool showColor = true;
-  
-  for (int i = 0; true; i++) {
+
+  while (true) {
+    //int audioPressure = analogRead(AUDIO_PIN);
     waveIndex++;
+    
     // switch show-color flag every n (= WAVE_LENGTH) iterations
     if(waveIndex % WAVE_LENGTH == 0) {
       showColor = !showColor;
+      waveIndex = 0; // reset wave index to 0 otherwise the integer would get too large
     }
-    
-    shiftLights(showColor);
-    turnOnPixels(color);
-    delay(1);
+
+    // shift values in lights array to the right
+    shiftLights(waveIndex, showColor, color);
+
+    // turn on pixels
+    turnOnPixels();
+    delay(MAX_DELAY/audioPressure);
   }
 }
 
@@ -53,16 +63,30 @@ void showWave(uint32_t color) {
  * shift lights array to the right
  * this produces the wave effect
  */
-void shiftLights(bool showColor) {
+void shiftLights(int waveIndex, bool showColor, uint32_t color) {
   for (int i = NUM_LEDS - 1; i > 0; i--) {
     lights[i] = lights[i-1];
   }
-
+    
   if (showColor) {
-    lights[0] = 1;
+    lights[0] = calculateColor(waveIndex, color);
   } else {
     lights[0] = 0;
   }
+}
+
+/*
+ * calculate color of current LED
+ */
+uint32_t calculateColor(int waveIndex, uint32_t color) {
+  
+  // 150 / WAVE_LENGTH = reduction per iteration
+  // lights[i] equals the position in the wave
+
+  // pos 0 in wave: no reduction
+  // pos n in wave: reduction for each step times n
+
+  return color;
 }
 
 /*
@@ -77,18 +101,18 @@ void turnOffPixels()
 }
 
 /*
- * sets strip color based on parameter
+ * sets strip color based on lights array
+ * the brightness is determined by the position of the LED in a wave
+ * 
+ * edges are darker than the middle
+ * this "smoothes" the wave effect
  */
-void turnOnPixels(uint32_t color)
-{  
+void turnOnPixels()
+{   
   for (int i = 0; i < NUM_LEDS; i++) {
-    Serial.write("a");
     // turn on or off lights based on value in array
-    if(lights[i] == 1) {
-      strip.setPixelColor(i, color);
-    } else {
-      strip.setPixelColor(i, strip.Color(0, 0, 0));
-    }
+    strip.setPixelColor(i, lights[i]);
   }
+    
   strip.show();
 }
